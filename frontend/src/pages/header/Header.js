1cc24c91c42c65dd45b661958830a26012f7cb51
ME Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2025-12-14 00:25:08 -0800

    Updating Book Library

--- frontend/src/pages/header/Header.js
+++ frontend/src/pages/header/Header.js
@@ -62,7 +62,7 @@ export default function Header({ toggleDarkMode, isDarkMode }){
                          Admin
                    </Typography>
                      
-                   <Button component={Link} to="/admin/dashboard" color="inherit" sx={{":hover":{backgroundColor:'black'}}}>Dashboard</Button>
+                   <Button component={Link} to="/admin/dashboard" color="inherit" sx={{":hover":{backgroundColor:'black'}}}>Books</Button>
                    <Button component={Link} to="/admin/book/post" color="inherit" sx={{":hover":{backgroundColor:'black'}}}>Add Book</Button>
 
                    <Button onClick={handleLogOut} color="inherit" sx={{":hover":{backgroundColor:'black'}}}>log out</Button>
