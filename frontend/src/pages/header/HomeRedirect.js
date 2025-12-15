c2cadf0bc0898e88c5e870bcd1530ca1afbe2aad
ME Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2025-12-14 23:07:22 -0800

    Updating Book Library

---
+++ frontend/src/pages/header/HomeRedirect.js
@@ -0,0 +1,16 @@
+import { Navigate } from "react-router-dom";
+import { isAdminLoggedIn, isStudentLoggedIn } from '../../utils/common';
+
+const HomeRedirect = ({ children }) => {
+  if (isAdminLoggedIn()) {
+    return <Navigate to="/admin/dashboard" replace />;
+  }
+
+  if (isStudentLoggedIn()) {
+    return <Navigate to="/student/dashboard" replace />;
+  }
+
+  return children;
+};
+
+export default HomeRedirect;
