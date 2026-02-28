15c0a6a901a09202adef31aa7313bf3939d203e7
ME Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2026-02-28 04:44:59 -0800

    Updating Book Library

--- frontend/src/pages/header/Home.js
+++ frontend/src/pages/header/Home.js
@@ -104,7 +104,7 @@ export default function Home() {
                       .button-container {
                         flex-direction: column;  /* stack vertically */
                         gap: 15px;
-                        margin-top: 2rem;
+                        margin-top: 2.3rem;
                       }
                     
                       .auth-button {
